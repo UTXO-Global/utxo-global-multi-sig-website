057bf1efc9d275c8b5d6483c99a8e39a54a19100
yangiv <khoahua.yiv@gmail.com>
2024-07-24 10:13:52 +0700

    init source

---
+++ configs/common.ts
@@ -0,0 +1,37 @@
+export const SITE_TITLE = "";
+export const SITE_DESCRIPTION = "";
+export const SITE_URL = "";
+export const SITE_IMAGE_URL =
+  "https://config.utxo.global/images/preview-site.png";
+export const CONTACT_MAIL = "contact@utxo.global";
+export const EXTENTSION_GITHUB =
+  "https://github.com/UTXO-Global/utxo-wallet-extension";
+export const SUBSTACK_LINK = "https://utxoglobal.substack.com/";
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACIKING_ID;
+
+export const NAVIGATIONS = [
+  {
+    id: "about-us",
+    label: "About Us",
+    href: "/#about-us",
+    isExternal: false,
+  },
+  {
+    id: "features",
+    label: "Features",
+    href: "/#features",
+    isExternal: false,
+  },
+  {
+    id: "contact",
+    label: "Contact",
+    href: "/#contact",
+    isExternal: false,
+  },
+  {
+    id: "github",
+    label: "GitHub",
+    href: EXTENTSION_GITHUB,
+    isExternal: true,
+  },
+];
---
+++ utils/helpers.ts
@@ -0,0 +1,5 @@
+import { toast } from "react-toastify";
+
+export const comingSoonMsg = () => {
+  toast.info("Coming Soon!");
+};
