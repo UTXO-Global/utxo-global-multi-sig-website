46b5854e4a2d113c90d51ddd2e90000c9258ff7f
yangiv <khoahua.yiv@gmail.com>
2024-07-30 14:04:52 +0700

    chore(list-account): add ui edit name

--- components/ListAccounts/Account/index.tsx
+++ components/ListAccounts/Account/index.tsx
@@ -1,13 +1,74 @@
 /* eslint-disable @next/next/no-img-element */
 import { useState } from "react";
-import { Popover } from "antd";
+import { Popover, Modal } from "antd";
 
+import Button from "@/components/Common/Button";
 import IcnThreeDots from "@/public/icons/icn-three-dots.svg";
 import IcnPencil from "@/public/icons/icn-pencil.svg";
 import IcnTrash from "@/public/icons/icn-trash.svg";
+import IcnTimesCircle from "@/public/icons/icn-times-circle.svg";
+
+const EditName = ({
+  isModalOpen,
+  setIsModalOpen,
+}: {
+  isModalOpen: boolean;
+  setIsModalOpen: (val: boolean) => void;
+}) => {
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <Modal
+        open={isModalOpen}
+        centered={true}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={false}
+        width={500}
+      >
+        <div className="bg-light-100 rounded-[16px] p-6 pt-10">
+          <h6 className="text-[20px] leading-[28px] text-dark-100 font-bold text-center">
+            Edit Name
+          </h6>
+          <div className="p-4 bg-grey-300 rounded-lg flex gap-4 items-center mt-8">
+            <input
+              type="text"
+              className="border-none outline-none bg-transparent flex-1 text-base"
+              placeholder="CKB Partners"
+            />
+            <IcnTimesCircle className="w-4 cursor-pointer" />
+          </div>
+          <div className="p-4 rounded-lg bg-grey-300 text-[16px] leading-[20px] text-dark-100 mt-4">
+            ckt1qzda0cr08m85hc8jlnfxdfrgurdas8m85hc8jlnfxdfrgurd..
+          </div>
+          <div className="mt-6 grid grid-cols-2 gap-6">
+            <Button kind="secondary" fullWidth onClick={handleCancel}>
+              Cancel
+            </Button>
+            <Button fullWidth onClick={handleCancel}>
+              Save
+            </Button>
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
+};
 
 const Actions = () => {
   const [open, setOpen] = useState(false);
+  const [isShowEditName, setIsShowEditName] = useState<boolean>(false);
 
   const hide = () => {
     setOpen(false);
@@ -19,7 +80,13 @@ const Actions = () => {
 
   const content = (
     <div className="py-1 bg-light-100 text-[14px] leading-[20px] rounded-lg overflow-hidden font-medium text-dark-100">
-      <div className="px-4 py-1 flex gap-2 hover:bg-grey-300 transition-colors items-center cursor-pointer">
+      <div
+        className="px-4 py-1 flex gap-2 hover:bg-grey-300 transition-colors items-center cursor-pointer"
+        onClick={() => {
+          setIsShowEditName(true);
+          hide();
+        }}
+      >
         <IcnPencil className="fill-success-100 w-4" />
         <p>Rename</p>
       </div>
@@ -31,15 +98,21 @@ const Actions = () => {
   );
 
   return (
-    <Popover
-      content={content}
-      placement="bottomLeft"
-      trigger="click"
-      open={open}
-      onOpenChange={handleOpenChange}
-    >
-      <IcnThreeDots className="w-5 cursor-pointer" />
-    </Popover>
+    <>
+      <Popover
+        content={content}
+        placement="bottomLeft"
+        trigger="click"
+        open={open}
+        onOpenChange={handleOpenChange}
+      >
+        <IcnThreeDots className="w-5 cursor-pointer" />
+      </Popover>
+      <EditName
+        isModalOpen={isShowEditName}
+        setIsModalOpen={(val) => setIsShowEditName(val)}
+      />
+    </>
   );
 };
 
@@ -57,8 +130,12 @@ const Account = () => {
             2/2
           </div>
         </div>
-
-        <p className="text-base font-medium text-dark-100">CKB Account</p>
+        <div>
+          <p className="text-base font-medium text-dark-100">CKB Account</p>
+          <p className="text-[14px] leading-[24px] text-grey-400">
+            <span className="text-dark-100">Pud:</span> ckt1qzda...rt7rr
+          </p>
+        </div>
       </div>
       <div className="gap-5 flex items-center">
         <div className="flex gap-2 items-center">
