d0423cd55eb8354e3f3ec7aad71b9b3a72e386a2
yangiv <khoahua.yiv@gmail.com>
2024-08-07 08:29:44 +0700

    chore(token-assets): add no cors

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -97,6 +97,8 @@ export const getBalanceMultiSigAccount = async (
     const res = await fetch(
       `${EXPLORER_API}/api/v1/addresses/${signers[i].signer_address}`,
       {
+        method: "GET",
+        mode: "no-cors",
         headers: {
           Accept: "application/vnd.api+json",
           "Content-Type": "application/vnd.api+json",
